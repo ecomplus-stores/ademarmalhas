ead54e201401b2977c61295578f5322224b5eb37
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-03-24 20:53:15 -0300

    chore: update form for cnpj

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,7 +3,8 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/AccountForm.html': path.resolve(__dirname, 'template/js/custom-js/html/AccountForm.html'),
+      './html/AccountForm.html': path.resolve(__dirname, 'template/js/custom-js/html/AccountForm.html')
+      //'./js/AccountForm.js': path.resolve(__dirname, 'template/js/custom-js/js/AccountForm.js')
     }
   }
 })
